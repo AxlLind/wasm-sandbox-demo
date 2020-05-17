53d1d4b1cff8e744ce7b73a8baf70bdcfb9566b1
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-05-17 10:39:02 +0200

    Change boardToWasmArray to use bitwise operations

--- frontend/src/Game/Bot.js
+++ frontend/src/Game/Bot.js
@@ -4,26 +4,23 @@ import request from 'superagent';
 // Sending complicated objects to WASM is annoying.
 // This is a demo so lets just cram the board state
 // into a single i32 and pass that to the module.
-const boardToWasmArray = board => {
-  // convert to 'binary' values
-  const wasmArray = board
-    .map(box => {
-      if (box === null) return '00';
-      if (box === 'X') return '01';
-      if (box === 'O') return '10';
-      return '11';
+const boardToWasmArray = board =>
+  board
+    .map((box, i) => {
+      let val = 3;
+      if (box === null) val = 0;
+      if (box === 'X') val = 1;
+      if (box === 'O') val = 2;
+      return val << (i * 2);
     })
-    .reverse();
-  // parse string as binary number
-  return parseInt(wasmArray.join(''), 2);
-};
+    .reduce((acc, val, i) => acc | val, 0);
 
 // Loads the wasm module from a base64 string
 const loadWasmModule = async base64 => {
   const bytes = decode(base64);
   const { instance } = await WebAssembly.instantiate(bytes);
   if (!instance.exports.makeMove) {
-    console.log('No makeMove function');
+    console.error('No makeMove function');
     return null;
   }
   return instance.exports;
