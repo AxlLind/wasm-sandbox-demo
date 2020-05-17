d4ec843d3319dbc65dfd97d7d9421c6b6eee9a02
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-05-17 10:34:54 +0200

    Merge App.js into index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CssBaseLine from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import StartView from './StartView';
+import PlayView from './PlayView';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      light: '#649568',
+      main: '#149414',
+      dark: '#0e6b0e',
+    },
+  },
+  typography: { fontFamily: 'Hack, monospace' },
+});
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <CssBaseLine />
+    <Router>
+      <Switch>
+        <Route path="/play" component={PlayView} />
+        <Route path="/" component={StartView} />
+      </Switch>
+    </Router>
+  </ThemeProvider>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
