7d5d3d31430119a28b768406a1eda8f7d06e185f
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-05-20 16:59:46 +0200

    Use material UI in Game.jsx

--- frontend/src/PlayView.jsx
+++ frontend/src/PlayView.jsx
@@ -4,16 +4,9 @@ import Game from './Game/Game';
 
 function PlayView() {
   const query = new URLSearchParams(useLocation().search);
-
-  // Get the bots from the url params
-  const xPlayer = query.get('xPlayer');
-  const oPlayer = query.get('oPlayer');
-  return (
-    <div>
-      <div>What a thing</div>
-      <Game xPlayer={xPlayer} oPlayer={oPlayer} />
-    </div>
-  );
+  const xPlayer = query.get('x');
+  const oPlayer = query.get('o');
+  return <Game xPlayer={xPlayer} oPlayer={oPlayer} />;
 }
 
 export default PlayView;
