0510b69b42bbffbf9c5ced1f05e8656a700b7548
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2020-05-22 11:33:24 +0200

    Add bot explanation

--- bot-examples/optimal.rs
+++ bot-examples/optimal.rs
@@ -1,4 +1,12 @@
-#![allow(non_snake_case)]
+/*
+  Optimal bot using the negamax algorithm to find the best move.
+  Prioritizes winning early if possible.
+  See: https://en.wikipedia.org/wiki/Negamax
+
+  By keeping the board representation in a single int the bot
+  is incredibly fast. It finishes an exhaustive search of an
+  empty board in less than 50ms even in the browser!
+*/
 const WINNING_COMBINATIONS: [[i32;3];8] = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 const EMPTY: i32 = 0;
 
@@ -11,8 +19,6 @@ fn set(board: i32, i: i32, v: i32) -> i32 {
   (board & !mask) | (v << (i * 2))
 }
 
-// Negamax algorithm to find the best move
-// See: https://en.wikipedia.org/wiki/Negamax
 fn negamax(board: i32, player: i32, depth: i32) -> (i32,i32) {
   let winner = WINNING_COMBINATIONS.iter()
     .map(|&[a,b,c]| [get(board,a), get(board,b), get(board,c)])
@@ -36,6 +42,7 @@ fn negamax(board: i32, player: i32, depth: i32) -> (i32,i32) {
 }
 
 #[no_mangle]
+#[allow(non_snake_case)]
 pub extern "C" fn makeMove(board: i32) -> i32 {
   negamax(board, 1, 0).0
 }
